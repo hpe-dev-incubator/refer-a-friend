c7e879d96fb8ce4a6e04d3da673d17cbeebcfa5b
iankbovard <you@example.com>
2018-11-12 01:27:45 -0600

    removed claimedprize column from referrer model

--- server/models/referrers.js
+++ server/models/referrers.js
@@ -18,11 +18,6 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 0
-    },
-    prizeWinner : {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
     }
   });
   Referrers.associate = (models) => {
