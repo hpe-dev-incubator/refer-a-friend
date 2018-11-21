ffefcc25d719844cc2450ae9ae66c92dd8e66627
iankbovard <you@example.com>
2018-11-21 12:15:27 -0600

    correct use of env variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,24 +1,24 @@
 const dotenv = require('dotenv').config();
 module.exports = {
     "development": {
-      "username": process.env.DB_USERNAME_DEV,
-      "password": process.env.DB_PASSWORD_DEV,
-      "database": process.env.DB_NAME_DEV,
-      "host": process.env.DB_HOST_DEV,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
       "dialect": "postgres"
     },
     "test": {
-      "username": process.env.DB_USERNAME_TEST,
-      "password": process.env.DB_PASSWORD_TEST,
-      "database": process.env.DB_NAME_TEST,
-      "host": process.env.DB_HOST_TEST,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
       "dialect": "postgres"
     },
     "production": {
-      "username": process.env.DB_USERNAME_PROD,
-      "password": process.env.DB_PASSWORD_PROD,
-      "database": process.env.DB_NAME_PROD,
-      "host": process.env.DB_HOST_PROD,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
       "dialect": "postgres"
   }
 }
