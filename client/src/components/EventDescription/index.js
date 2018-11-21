a33423b466e6bde7281fc19b38f652734dace783
iankbovard <you@example.com>
2018-11-21 15:22:06 -0600

    added eventdescription

--- client/src/components/EventDescription/index.js
+++ client/src/components/EventDescription/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Paragraph } from 'grommet';
+import { Box, Paragraph, Anchor } from 'grommet';
 import { Heading } from './styles';
 import GremlinWave from '../GremlinWave';
 import Logo from '../Logo';
@@ -23,8 +23,14 @@ const EventDescription = () => (
       <Paragraph
         size="large"
       >
-        Turnip greens yellow ricebean rutaba endive cauliflower sea lettuce kohlrabi amaranth water spincah avocado daikon napa cabbage asparagus winter purslane kale.
+        Participate in the Refer a Hacker challenge to win cool prizes! All you need to do is sign up here, refer three people to the Hack Shack to participate in a challenge. Once all three people have participated in a Hack Shack event and have checked in via this app you'll get an email notifying you to come pick up your awesome prize.
       </Paragraph>
+      <Anchor 
+        label="Check out the Hack Shack agenda" 
+        href="https://content.attend.hpe.com/go/agendabuilder.sessions/?l=38&locale=en_US&AEID=&selectedFilters=tag_0:0,tag_217:3723&kw=hack%20shack"
+        target="_blank"
+        rel="nofollow noreferrer"
+      />
     </Box>
     </Box>
     <Box
