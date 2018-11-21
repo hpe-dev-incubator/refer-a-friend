47e9f7900cd2c1f581e3102cc8e98acc774081c6
karatehcops <contact@lazers.tv>
2018-11-21 13:10:45 -0500

    Update email messaging.

--- server/index.js
+++ server/index.js
@@ -11,11 +11,12 @@ const sendGrid = require('./sendGrid');
 
 const app = express();
 
-const emailSubjectRef = 'Youve been invited!';
-const emailBodyRef = 'Come hack with your pal!';
+const emailSubjectRef = 'You\'ve been invited to the HPE Discover Madrid Hack Shack!';
+const emailBodyRef = (name, userEmail) =>
+  `Join your pal, <a href="mailto:${userEmail}">${name}</a>, at HPE Discover booth DEMO1404, A.K.A the Hack Shack. If you enter a code challenge or participate in a design or technical workshop and check-in via our Refer a Friend app ${name} will have a chance to win cool prizes! You can find more information about our Hack Shack sessions at the <a href="https://content.attend.hpe.com/go/agendabuilder.sessions/?l=38&locale=en_US&AEID=&selectedFilters=tag_0:0,tag_217:3723&kw=hack%20shack">HPE Discover agenda builder</a>`;
 
-const emailSubjectWin = 'You Won!';
-const emailBodyWin = 'Great job on having friends that follow through!';
+const emailSubjectWin = 'Come get your prize at the HPE DEV Hack Shack!';
+const emailBodyWin = 'Looks like you referred the right hackers, they have all checked in and the prize is yours to claim. Come to the Hack Shack, DEMO1404, to collect your prize.';
 
 app.use(bodyParser.json());
 
@@ -51,11 +52,11 @@ app.post('/api/refer-a-friend', (req, res) => {
     }
   })
   .then(() => {
-    Referrers.create({ email: email, name: name })
-    .then(referrer => {
+    Referrers.create({ email, name })
+    .then(({ id, email: referrerEmail, name }) => {
       referrals.map(email => {
-        sendGrid({ recipient: email, subject: emailSubjectRef, content: emailBodyRef })
-        Referrals.create({ email: email, referrerId: referrer.id })
+        sendGrid({ recipient: email, subject: emailSubjectRef, content: emailBodyRef(name, referrerEmail) })
+        Referrals.create({ email: email, referrerId: id })
       })
     })
     .then(() => res.send({ success: true }));
