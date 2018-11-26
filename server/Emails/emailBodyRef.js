01bd2efc27506944c5167ef65a45c7d93319f2a3
britt6612 <42451602+britt6612@users.noreply.github.com>
2018-11-26 15:56:17 -0700

    madde functions for the emails and html passed as a string

---
+++ server/Emails/emailBodyRef.js
@@ -0,0 +1,209 @@
+const emailBodyRef = (name, userEmail) =>
+  `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  
+  <head>
+    <title>HPE DEV Newsletter</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  
+  <body style="min-width:640px;margin: 0 0 0 0; -webkit-font-smoothing: antialiased;" bgcolor="#f6f6f6" link="#003366" vlink="#003366" alink="#003366" text="#000000">
+  
+    <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#f6f6f6">
+      <tr>
+        <td bgcolor="#f6f6f6">
+  
+          <table width="640" style="min-width:640px;" cellspacing="0" cellpadding="0" border="0" align="center">
+            <tbody>
+              <tr>
+                <td height="0" style="line-height:0px;font-size:0px;padding:0;margin:0;max-height:0px;">
+                  <a name="eiel1694740">
+                    <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="0" style="display:block" alt="?asset=EM_TXT_H" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td width="640" height="5" style="line-height:0px;font-size:0px;">
+                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="5" alt="?asset=EM_TXT_H" style="display:block; border:0;"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+                    <tbody>
+                      <tr>
+                        <td width="32" style="line-height:0px;font-size:0px;">
+                          <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="32" height="1" alt="?asset=EM_TXT_H" style="display:block; border:0;"
+                          />
+                        </td>
+                        <td width="32" style="line-height:0px;font-size:0px;">
+                          <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="32" height="1" alt="" style="display:block; border:0;" />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              <tr>
+                <td width="640" height="5" style="line-height:0px;font-size:0px;">
+                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="5" alt="" style="display:block; border:0;" />
+                </td>
+              </tr>
+              <tr>
+                <td bgcolor="#425563">
+                  <table width="640" bgcolor="#425563" cellspacing="0" cellpadding="0" border="0" align="center">
+                    <tbody>
+                      <tr>
+                        <td width="600" height="10" bgcolor="#425563" style="line-height:0px;font-size:0px;">
+                          <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="10" alt="" style="display:block; border:0;" />
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <table width="640" cellspacing="0" cellpadding="0" border="0" align="center">
+                            <tbody>
+                              <tr>
+                                <td width="32" style="line-height:0px;font-size:0px;">
+                                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="32" height="1" alt="" style="display:block; border:0;" />
+                                </td>
+                                <td width="145">
+                                  <a href="https://www.hpe.com">
+                                    <img style="display:block;" src="https://marketing-image-production.s3.amazonaws.com/uploads/188b64203941b85670ad820e376c2c3d747bc6040dc020c09a1275e29eaf02ac7faa8143025e1df8997037e1cc0425497799997e911acc8cd69f28101c31261d.gif" width="145" height="63" alt="Hewlett Packard Enterprise logo"
+                                      border="0" />
+                                  </a>
+                                </td>
+                                <td width="30">
+                                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="30" height="1" alt="" style="display:block; border:0;" />
+                                </td>
+                                <td width="425" align="right">
+                                  <font style="font-family: arial; font-size:14px;">
+                                    &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+                                  </font>
+                                </td>
+                                <td width="32" style="line-height:0px;font-size:0px;">
+                                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="32" height="1" alt="" style="display:block; border:0;" />
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td width="600" height="10" bgcolor="#425563" style="line-height:0px;font-size:0px;">
+                          <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="10" alt="" style="display:block; border:0;" />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+  
+          <table bgcolor="#4b5e6b" width="640" cellspacing="0" cellpadding="0" border="0" align="center">
+            <tbody>
+              <tr>
+                <td height="5" style="line-height:0px;font-size:0px;padding:0;margin:0;max-height:0px;background-color:#00B388;">
+                  <a name="eiel1694745">
+                    <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="5" style="display:block" alt="" />
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+  
+          <table bgcolor="#4b5e6b" width="640" cellspacing="0" cellpadding="0" border="0" align="center">
+            <tbody>
+              <tr>
+                <td>
+                  <a href="https://developer.hpe.com" target="_blank">
+                    <img src="https://marketing-image-production.s3.amazonaws.com/uploads/b1465ea55d65a390cb201731d5c9ae0efc0e8658967323f28141a0bcda7c5b0f5576703b4dd9a6c47b80804a3707ee65fce0e94a77f59bf446cac98c5cc2f9fa.jpg" width="640" height="432" alt="HPE DEV icon" border="0" style="display:block; border:0; max-width:640px; max-height:432px;"
+                    />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td bgcolor="#4b5e6b">
+                  <table width="640" bgcolor="#4b5e6b" cellspacing="0" cellpadding="0" border="0" align="center">
+                    <tbody>
+                      <tr>
+                        <td width="32" style="line-height:0px;font-size:0px;">
+                          <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="32" height="1" alt="" style="display:block; border:0;" />
+                        </td>
+                        <td>
+                          <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+                            <tbody>
+                              <tr>
+                                <td width="560" height="32" style="line-height:0px;font-size:0px;">
+                                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="32" alt="" style="display:block; border:0;" />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td align="left">
+                                  <font style="font-family:arial;font-size:42px;color:#FFFFFF;text-decoration:none;font-weight:bold;">
+                                    Congratulations
+                                  </font>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td width="560" height="10" style="line-height:0px;font-size:0px;">
+                                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="10" alt="" style="display:block; border:0;" />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td align="left">
+                                  <font style="font-family:arial;font-size:18px;color:#FFFFFF;line-height:24px;">
+                                    Join your pal, <a href="mailto:${userEmail}">${name}</a>, at HPE Discover booth
+                                     DEMO1404, A.K.A the Hack Shack. If you enter a code challenge or participate
+                                     in a design or technical workshop and check-in via our Refer a Friend
+                                     app ${name} will have a chance to win cool prizes! You can find
+                                     more information about our Hack Shack sessions at the
+                                     <a href="https://content.attend.hpe.com/go/agendabuilder.sessions/?l=38&locale=en_US&AEID=&selectedFilters=tag_0:0,tag_217:3723&kw=hack%20shack">
+                                     HPE Discover agenda builder</a>
+                                    <br/><br/>
+    
+                                  </font>
+                                </td>
+                              </tr>
+                              <tr>
+                                <td width="560" height="32" style="line-height:0px;font-size:0px;">
+                                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="32" alt="" style="display:block; border:0;" />
+                                </td>
+                              </tr>
+                              
+                            </tbody>
+                          </table>
+                        </td>
+                        <td width="32" style="line-height:0px;font-size:0px;">
+                          <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="32" height="1" alt="" style="display:block; border:0;" />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+              
+            </tbody>
+          </table>
+  
+          <table width="640" cellspacing="0" cellpadding="0" border="0" align="center">
+            <tbody>
+              <tr>
+                <td width="640" height="5" style="line-height:0px;font-size:0px; background-color:#00B388;">
+                  <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="5" alt="" style="display:block; border:0;" />
+                </td>
+              </tr>
+              <tr>
+                <td height="0" style="line-height:0px;font-size:0px;padding:0;margin:0;max-height:0px;">
+                  <a name="eiel1694746">
+                    <img src="https://marketing-image-production.s3.amazonaws.com/uploads/9e8c3e1ef8e2c062b50ebbaa97e66162f2c2991a48c0cac45a2611b740dbf30c9e3c11c962ce5f75fb3a681526eb0037b5f0d747b293f6dc7d3a98541234f693.gif" width="1" height="0" style="display:block" />
+                  </a>
+                </td>
+              </tr>
+        </td>
+      </tr>
+    </table>
+  </body>
+  </html>`;
+module.exports = emailBodyRef;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const emailBodyRef = require('.Emails')
+const emailBodyWin = require('.Emails')
 const dotenv = require('dotenv').config();
 const port  = process.env.PORT || 3000;
 
@@ -12,11 +14,9 @@ const sendGrid = require('./sendGrid');
 const app = express();
 
 const emailSubjectRef = 'You\'ve been invited to the HPE Discover Madrid Hack Shack!';
-const emailBodyRef = (name, userEmail) =>
-  `Join your pal, <a href="mailto:${userEmail}">${name}</a>, at HPE Discover booth DEMO1404, A.K.A the Hack Shack. If you enter a code challenge or participate in a design or technical workshop and check-in via our Refer a Friend app ${name} will have a chance to win cool prizes! You can find more information about our Hack Shack sessions at the <a href="https://content.attend.hpe.com/go/agendabuilder.sessions/?l=38&locale=en_US&AEID=&selectedFilters=tag_0:0,tag_217:3723&kw=hack%20shack">HPE Discover agenda builder</a>`;
 
 const emailSubjectWin = 'Come get your prize at the HPE DEV Hack Shack!';
-const emailBodyWin = 'Looks like you referred the right hackers, they have all checked in and the prize is yours to claim. Come to the Hack Shack, DEMO1404, to collect your prize.';
+console.log(emailBodyWin);
 
 app.use(bodyParser.json());
 
